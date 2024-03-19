44d3b4b0d9d94dcf3c479612bf3f74ce9524229a
Norookezi <norookezi@pandacrp.com>
2024-03-19 09:58:32 +0100

    feat: ➕ Added Tailwind and HeroIcon

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'mina': ['"Mina"', "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+};
