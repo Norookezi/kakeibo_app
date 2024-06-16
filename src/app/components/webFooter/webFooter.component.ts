c0351875eaaacb41e7a4fb795a9754d07400a9c3
Norookezi <norookezi@pandacrp.com>
2024-06-16 21:25:22 +0200

    feat: ✨ Added footer for web app

---
+++ src/app/components/webFooter/webFooter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webFooter',
+  templateUrl: './webFooter.component.html',
+  styleUrls: ['./webFooter.component.css']
+})
+export class WebFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
