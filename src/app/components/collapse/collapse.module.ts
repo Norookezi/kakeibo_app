387675db4a010b9b79989186b6cf937594865095
Norookezi <norookezi@pandacrp.com>
2024-06-16 21:32:45 +0200

    fix: 🔥 Removed unused ressources

--- src/app/components/collapse/collapse.module.ts
+++ src/app/components/collapse/collapse.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CollapseComponent } from './collapse.component';
-import { TitleDirective } from './directive/title.directive';
-import { DetailsDirective } from './directive/details.directive';
 import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     CollapseComponent,
-    TitleDirective,
-    DetailsDirective
   ],
   imports: [
     CommonModule,
