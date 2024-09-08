4661e7b3535a2d9ad221192260641cb960893c1b
MickaelVotte <mickael.votte@hotmail.com>
2024-09-08 15:06:33 +0200

    debut dev popup budgetline

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -2,10 +2,10 @@ import { Component, Input } from '@angular/core';
 
 
 @Component({
-  selector: 'app-collapse',
+  selector: 'app-popup',
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.scss']
 })
 export class PopupComponent {
-  @Input({required: true}) title!: string;
+  
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ModalModule } from '@component/modal/modal.module';
 import { DashboardPage } from './dashboard.page';
 import { BtnModule } from '@component/-btn/btn.module';
+import { PopupModule } from '@component/popup/popup.module';
 
 @NgModule({
   declarations: [DashboardPage],
-  imports: [CommonModule, ModalModule, BtnModule],
+  imports: [CommonModule, ModalModule, BtnModule, PopupModule],
 })
 export class DashboardModule {}
